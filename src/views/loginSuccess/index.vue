1048c3a8ca16cda9e176dd76f77ef5428dd977ee
Cami Wang <iris0952@gmail.com>
2025-05-07 21:17:34 +0800

    調整路徑

--- src/views/loginSuccess/index.vue
+++ src/views/loginSuccess/index.vue
@@ -16,10 +16,12 @@ onMounted(async() => {
   // 從網址取得 JWT token
   const token = route.query.token as string;
   const id = route.query.id as string;
+  console.log('>>> OAuth callback, token =', token, 'id =', id)
   if (!token || !id) return router.replace('/')
 
   userStore.setToken(token)
   userStore.setUserId(id)
+  
   localStorage.setItem('access_token', token)
   localStorage.setItem('userId', id)
 
@@ -31,13 +33,13 @@ onMounted(async() => {
     userStore.setAvatar(res.data.profile_image_url)
     localStorage.setItem('avatarUrl', res.data.profile_image_url)
 
-    router.replace('/home');
+    router.replace('/Home');
   } catch (e) {
     console.error('取得完整用戶資料失敗', e)
-    router.replace('/login');
+    router.replace('/Home');
   }
 
-  router.replace('/home');
+
   
 });
 </script>
