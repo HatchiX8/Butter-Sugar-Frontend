0381b1650da83f749db8def50abd0c36b4c3ab14
Cami Wang <iris0952@gmail.com>
2025-05-11 18:55:02 +0800

    header 增加底線、搜尋框移除 border

--- src/components/layout/headerComps.vue
+++ src/components/layout/headerComps.vue
@@ -1,6 +1,6 @@
 <template>
-  <header class="font-['Noto Serif TC'] mx-auto w-full max-w-7xl px-6 py-3">
-    <div class="w-1200px flex items-center justify-between">
+  <header class="font-['Noto Serif TC'] px-6 py-3">
+    <div class="mx-auto w-full max-w-7xl flex items-center justify-between">
       <router-link to="/home">
         <img src="@/assets/logo.png" alt="Butter&Sugar" class="h-8" />
       </router-link>
@@ -8,12 +8,13 @@
       <nav class="flex items-center gap-4">
         <a href="#" class="text-white no-underline">探索課程</a>
         <div class="pos-relative flex items-center justify-between">
-          <n-input
-            type="text"
-            placeholder="輸入關鍵字搜尋"
-            class="w-300px h-48px border-none bg-[#0D0B07] leading-[48px] text-[#9E9E9E] focus:outline-none"
-            bordered="{false}"
-          />
+          <n-config-provider :theme-overrides="themeOverrides">
+            <n-input
+              type="text"
+              placeholder="輸入關鍵字搜尋"
+              class="w-300px h-48px  bg-[#0D0B07] leading-[48px] focus:outline-none"
+            />
+          </n-config-provider>
           <div
             class="i-ion:search-outline pos-absolute text-5 right-2 top-1/2 -translate-y-1/2 cursor-pointer text-[#9E9E9E]"
           ></div>
@@ -29,4 +30,21 @@
 
 <script setup lang="ts">
 import { userAction } from '@/components/index';
+
+const themeOverrides = {
+  Input: {
+    border: '1px solid  #0D0B07',
+    borderHover: '1px solid  #0D0B07',
+    borderFocus: '1px solid  #0D0B07',
+    borderRadius: '4px',
+    placeholderColor: '#9e9e9e',
+    color: '#9e9e9e',
+  }
+}
 </script>
+<style>
+header{
+  border-bottom: 1px solid rgba(253,253,253,0.25);
+}
+
+</style>
