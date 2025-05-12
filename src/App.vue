071c076c168e5110b05d4604521cc487b42dc7b4
Cami Wang <iris0952@gmail.com>
2025-05-12 11:23:53 +0800

    App.vue 新增 themeOverrides 可以全站預設基本字體、大小、粗細

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <template>
-  <router-view />
+  <n-config-provider :theme-overrides="themeOverrides">
+    <router-view />
+  </n-config-provider>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { NConfigProvider } from 'naive-ui'
+
+const themeOverrides = {
+  common: {
+    fontFamily: `'Noto Sans TC', Roboto, 'Helvetica Neue', Arial, sans-serif`,
+    fontSize: '14px',
+    fontWeightStrong: '400',
+  }
+}
+</script>
