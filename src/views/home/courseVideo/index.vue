a6f16c550cfea207de9668d021531bb289336688
Cami Wang <iris0952@gmail.com>
2025-06-04 15:23:15 +0800

    feat:新增課程影片資料夾

---
+++ src/views/home/courseVideo/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>上課頁面</div>
+</template>
+<script setup lang="ts">
+import breadcrumbComps from '@/components/layout/breadcrumbComps.vue';
+import typography from '@/components/layout/typography.vue';
+import selectComps from '@/components/layout/selectComps.vue';
+
+</script>
