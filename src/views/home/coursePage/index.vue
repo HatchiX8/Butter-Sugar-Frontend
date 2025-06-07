0be5e9825a7ace85851f14d563b4637e020aed92
Cami Wang <iris0952@gmail.com>
2025-06-08 01:17:17 +0800

    feat: 新增影片與選單

--- src/views/home/coursePage/index.vue
+++ src/views/home/coursePage/index.vue
@@ -1,6 +1,19 @@
 <template>
-  <div>上課頁面</div>
+  <div class="w-full max-w-[1280px] mx-auto px-4 flex flex-col items-start box-border text-white">
+    <breadcrumbComps
+      class="mt-30"
+      :items="[
+        { label: '首頁', to: '/' },
+        { label: '我的課程', to: '/explore' },
+        { label: '職人級！一次掌握歐式麵包的高水量與發酵秘訣' },
+      ]"
+    />
+    <courseVideo />
+    <qaModule />
+    <router-view />
+  </div>
 </template>
-<script setup lang="ts">
-
+<script lang="ts" setup>
+import breadcrumbComps from '@/components/layout/breadcrumbComps.vue';
+import courseVideo from './comps/courseVideo.vue';
 </script>
