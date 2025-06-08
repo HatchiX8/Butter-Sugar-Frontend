8974b291924a2077f8a6b0bdcd2dcd1955fb8e82
Cami Wang <iris0952@gmail.com>
2025-06-08 16:19:28 +0800

    fix: Update App.vue 刪除 themeOverrides
    刪除 themeOverrides

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,5 @@
 <template>
-  <n-config-provider :theme-overrides="themeOverrides">
-    <router-view />
-  </n-config-provider>
+  <router-view />
 </template>
 
-<script setup lang="ts">
-import { NConfigProvider } from 'naive-ui'
-
-const themeOverrides = {
-  common: {
-    fontFamily: `'Noto Sans TC', Roboto, 'Helvetica Neue', Arial, sans-serif`,
-    fontSize: '14px',
-    fontWeightStrong: '400',
-  }
-}
-</script>
+<script setup lang="ts"></script>
