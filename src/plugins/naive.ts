0acf3c69c4f98b6f53d81bc905ba3a738d6f864c
Cami Wang <iris0952@gmail.com>
2025-06-12 12:05:58 +0800

    feat: 新增 divider 樣式

--- src/plugins/naive.ts
+++ src/plugins/naive.ts
@@ -125,6 +125,12 @@ export function withNaiveProviders(App: Component) {
                 groupHeaderArrowColorActive: colors.primaryDefault,
                 groupHeaderArrowColorChildActive: colors.primaryDefault,
                 groupHeaderArrowColorChildActiveHover: colors.primaryDefault,
+                // 
+                itemColorHover: hexToRgba(colors.primaryDefault, 0.3),
+                itemColorActive: hexToRgba(colors.primaryDefault, 0.8),
+              },
+              Divider: {
+                color: hexToRgba(colors.primaryDefault, 1),
               },
             },
           },
