eb19444d3628644ea8edf3d12c6b58a0657348c1
emily <yiting50425@gmail.com>
2025-06-14 16:33:28 +0800

    refactor:購物車改用共用元件

--- src/views/home/cart/pages/cart/index.vue
+++ src/views/home/cart/pages/cart/index.vue
@@ -3,49 +3,17 @@
   <div class="text-white">
     <div class="max-w-6xl mx-auto px-4 py-8">
       <div class="flex gap-6">
-        <div class="flex-1 bg-neutral_600 border-1 border-solid border-white/10% p-4 rounded-0.5">
-          <div class="flex items-center justify-between p-4 mb-4">
-            <div>
-              <h2 class="font-bold text-6">購物車</h2>
-              <div class="mt-4 w-14 h-0.1 bg-primaryDefault"></div>
-            </div>
-            <div class="font-['Noto Sans TC'] text-primaryDefault text-4 text-right">總共 {{ itemCount }} 件</div>
-          </div>
+        <div v-if="itemCount === 0" class="flex-1 bg-neutral_600 border-1 border-solid border-white/10% p-4 rounded-0.5">
           <!-- 空購物車提示 -->
-          <div v-if="itemCount === 0" class="text-center text-sm text-neutral_200 py-8">
+          <div class="text-center text-sm text-neutral_200 py-8">
             <span>購物車是空的，快去挑選喜歡的課程吧！</span>
           </div>
-          <div v-for="(item, index) in cartItems" :key="item.course_id"
-            class="p-4">
-            <div class="flex gap-4 pb-4" :class="{ 'custom-border-bottom': index !== cartItems.length - 1 }">
-              <router-link
-                :to="`/home/course/${item.course_id}`"
-                class="flex flex-1 items-start gap-4 cursor-pointer no-underline group">
-                <!-- 課程圖片 -->
-                <div class="shrink-0 w-36 h-24 overflow-hidden rounded-md transition-shadow duration-300 group-hover:shadow-lg">
-                  <img :src="item.course_smallimage" alt="課程小圖"
-                    class="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105" />
-                </div>
-                <!-- 課程名稱 -->
-                <div class="font-bold text-white text-base leading-snug clamped-text">
-                  {{ item.course_name }}
-                </div>
-              </router-link>
-              <div class="w-21 min-w-28 text-right">
-                <div class="text-white font-bold text-lg mb-3">
-                  {{ formatCurrency(item.price) }}
-                </div>
-                <!-- 移除按鈕 -->
-                <baseButton text label="移除" @click="openConfirmModal(item)" icon="i-ion:trash-outline" iconPosition="right" iconClass="w-4 h-4 text-neutral_200 hover:text-primaryDefault" labelClass="text-neutral_200 hover:text-primaryDefault" />
-              </div>
-            </div>
-          </div>
         </div>
-        <!-- 移除課程確認彈窗 -->
-        <confirmModal
-          v-if="itemToDelete"
-          v-model:modelValue="showConfirmModal"
-          :item="itemToDelete"
+        <cartList
+          :cartItems="cartItems"
+          :itemCount="itemCount"
+          :orderDetails="false"
+          @removeItem="cartStore.removeItem"
         />
         <!-- 訂單資訊 -->
         <div class="bg-black px-4 py-8 rounded text-4 md:w-1/3 flex-shrink-0 flex flex-col h-1/4 sticky top-24">
@@ -73,23 +41,15 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
+import { onMounted } from 'vue';
 import { useCartStore } from '@/stores/models/cart/store';
 import { storeToRefs } from 'pinia';
 import { useRouter } from 'vue-router';
-import confirmModal from '@/views/home/cart/comps/confirmModal.vue';
 import baseButton from '@/components/layout/baseButton.vue';
+import cartList from '@/components/data/cartList.vue';
 
 const cartStore = useCartStore();
 const { cartItems, itemCount, totalPrice } = storeToRefs(cartStore);
-
-// 移除課程彈窗
-const itemToDelete = ref<{ course_id: string; course_name: string } | null>(null);
-const showConfirmModal = ref(false);
-const openConfirmModal = (item: { course_id: string; course_name: string }) => {
-  itemToDelete.value = item
-  showConfirmModal.value = true
-};
 
 const router = useRouter();
 const goToCheckout = () => {
