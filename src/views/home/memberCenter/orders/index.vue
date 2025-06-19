d5976a1f0397a39b7987ccf38ed4317b079f7a05
Cami Wang <iris0952@gmail.com>
2025-06-19 09:39:39 +0800

    style: 標題樣式修改

--- src/views/home/memberCenter/orders/index.vue
+++ src/views/home/memberCenter/orders/index.vue
@@ -7,7 +7,7 @@
         { label: '訂單紀錄' },
       ]"
     />
-    <typography variant="h2" font-type="title" underline class="mb-8">訂單紀錄</typography>
+    <typography variant="h2" font-type="title" underline class="mb-8 text-white">訂單紀錄</typography>
     <n-select v-model:value="value" :options="options" placeholder="所有訂單" class="w-20% mb-6 ml-auto"/>
       <n-data-table
         :columns="columns"
