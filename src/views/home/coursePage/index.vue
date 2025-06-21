87b816f13edf2d82a67697d66592f14c3d962b78
Cami Wang <iris0952@gmail.com>
2025-06-21 18:46:26 +0800

    fix: 調整麵包屑路徑

--- src/views/home/coursePage/index.vue
+++ src/views/home/coursePage/index.vue
@@ -4,7 +4,7 @@
       class="mt-20"
       :items="[
         { label: '首頁', to: '/' },
-        { label: '我的課程', to: '/explore' },
+        { label: '我的課程', to: '/home/my-course?page=1' },
         { label: '職人級！一次掌握歐式麵包的高水量與發酵秘訣' },
       ]"
     />
